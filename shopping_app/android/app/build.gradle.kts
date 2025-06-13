ac48d645df1fdcab0fac5b1c23bc99ec7c987f56
dothaituan <tuandothai522004@gmail.com>
2025-06-13 16:10:04 +0700

    Update build.gradle.kts

--- shopping_app/android/app/build.gradle.kts
+++ shopping_app/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "com.example.shopping_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "29.0.13599879"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
