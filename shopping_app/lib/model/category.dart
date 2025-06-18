91d416eca437fdea6153b82a18325e504f69ecf9
dothaituan <tuandothai522004@gmail.com>
2025-06-18 17:31:05 +0700

    add category model

---
+++ shopping_app/lib/model/category.dart
@@ -0,0 +1,42 @@
+class Category {
+  final int id;
+  final String name;
+  final String? description;
+  final bool isActive;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+
+  Category({
+    required this.id,
+    required this.name,
+    this.description,
+    required this.isActive,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      isActive: json['is_active'] ?? true,
+      createdAt: json['created_at'] != null 
+          ? DateTime.parse(json['created_at']) 
+          : null,
+      updatedAt: json['updated_at'] != null 
+          ? DateTime.parse(json['updated_at']) 
+          : null,
+    );
+  }
+    Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'is_active': isActive,
+      'created_at': createdAt?.toIso8601String(),
+      'updated_at': updatedAt?.toIso8601String(),
+    };
+  }
+}
