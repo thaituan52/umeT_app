0fe43df2a7a3432ac830249f3c7d55e94965e5d2
dothaituan <tuandothai522004@gmail.com>
2025-06-23 15:28:21 +0700

    Update check_all.sql

--- sql/check_all.sql
+++ sql/check_all.sql
@@ -3,4 +3,6 @@ USE shopping_app;
 SELECT * FROM user_info;
 SELECT * FROM categories;
 SELECT * FROM products;
-SELECT * FROM product_categories
+SELECT * FROM product_categories;
+SELECT * FROM orders;
+SELECT * FROM order_items;
