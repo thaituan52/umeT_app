3c1c19e80c6656cafba1349f90b4d295d790b740
dothaituan <tuandothai522004@gmail.com>
2025-06-24 11:01:55 +0700

    adding comment for backend.py

--- shopping_app/lib/backend_api/backend.py
+++ shopping_app/lib/backend_api/backend.py
@@ -423,7 +423,7 @@ def update_product(db: Session, product_id: int, product_update: ProductUpdate):
     return db_product
 
 
-#maynot use
+# Order/ OrderItem helper function
 def get_order_by_id(db: Session, order_id: int):
     return db.query(Order).filter(Order.id == order_id).first()
 
@@ -434,13 +434,14 @@ def get_user_cart(db: Session, user_id: int):
     return db.query(Order).filter(Order.user_id == user_id, Order.status == 1).first()
 
 
-#may need OrderItem.order_id to be unique
+# may need OrderItem.order_id to be unique
 def calculate_order_total(db: Session, order_id: int):
     items = db.query(OrderItem).filter(OrderItem.order_id == order_id)
     total = sum(float(item.price_per_unit) * item.quantity for item in items)
     return total
 
 def create_order(db: Session, order: OrderCreate):
+    #Create the order and retrieve items data
     order_data = order.model_dump()
     items_data = order_data.pop('items', [])
 
@@ -449,9 +450,11 @@ def create_order(db: Session, order: OrderCreate):
     db.commit()
     db.refresh(db_order)
 
+    #Add items to the order
     total_amount = 0.0
 
     for item_data in items_data:
+        #Verify product exists
         product = get_product_by_id(db, item_data['product_id'])
         if not product:
             db.rollback()
@@ -470,7 +473,7 @@ def create_order(db: Session, order: OrderCreate):
         db.add(db_item)
         total_amount += float(price_per_unit) * item_data['quantity']
 
-    
+    # Update order total
     db_order.total_amount = str(total_amount)
     db.commit()
     db.refresh(db_order)
